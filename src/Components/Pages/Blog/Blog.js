40a7a7fee2bd0e767e94a6f116dd4a532f429c3f
jinx71 <karim.sazed@gmail.com>
2022-11-08 05:45:23 +0600

    Blog Page Build

--- src/Components/Pages/Blog/Blog.js
+++ src/Components/Pages/Blog/Blog.js
@@ -3,7 +3,33 @@ import React from 'react';
 const Blog = () => {
     return (
         <div>
-            This is blog
+            <div className="card border w-3/4 mx-auto mt-20 shadow-xl">
+                <div className="card-body">
+                    <code className="card-title"><span>1.</span>Write down the differences between SQL and NoSQL</code>
+                    <p><span className='font-bold'>Answer: </span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi incidunt quibusdam perferendis porro hic. Animi praesentium ad magnam harum velit.</p>
+
+                </div>
+            </div>
+            <div className="card border w-3/4 mx-auto mt-10 shadow-xl">
+                <div className="card-body">
+                    <code className="card-title"><span>2.</span>What is JWT, and how does it work?</code>
+                    <p><span className='font-bold'>Answer: </span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi incidunt quibusdam perferendis porro hic. Animi praesentium ad magnam harum velit.</p>
+
+                </div>
+            </div>
+            <div className="card border w-3/4 mx-auto mt-10 shadow-xl">
+                <div className="card-body">
+                    <code className="card-title"><span>3.</span>What is the difference between javascript and NodeJS?</code>
+                    <p><span className='font-bold'>Answer: </span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi incidunt quibusdam perferendis porro hic. Animi praesentium ad magnam harum velit.</p>
+
+                </div>
+            </div>
+            <div className="card border w-3/4 mx-auto mt-10 shadow-xl">
+                <div className="card-body">
+                    <code className="card-title"><span>4.</span>How does NodeJS handle multiple requests at the same time?</code>
+                    <p><span className='font-bold'>Answer: </span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi incidunt quibusdam perferendis porro hic. Animi praesentium ad magnam harum velit.</p>
+                </div>
+            </div>
         </div>
     );
 };
