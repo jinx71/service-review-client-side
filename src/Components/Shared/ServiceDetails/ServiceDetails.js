6ae5ab9b97c14a6cb86bd32624d5b31a9b6701f8
jinx71 <karim.sazed@gmail.com>
2022-11-09 05:21:01 +0600

    Photo gallary build

--- src/Components/Shared/ServiceDetails/ServiceDetails.js
+++ src/Components/Shared/ServiceDetails/ServiceDetails.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import { useLoaderData } from 'react-router-dom';
-import ServiceDetailsBanner from '../ServiceDetailsBanner/ServiceDetailsBanner';
+import Gallary from '../Gallary/Gallary';
 
 const ServiceDetails = () => {
     const serviceDetails = useLoaderData()
     // console.log(serviceDetails)
     return (
-        <>
-
-            <div className='col-span-1 md:col-span-2 lg:col-span-4'>
-                this is service details
+        <div className='col-span-1 md:col-span-2 lg:col-span-4 mt-20'>
+            <div>
+                <img src={serviceDetails.image} alt="" />
             </div>
-        </>
+            <Gallary photos={serviceDetails.galaryImage}></Gallary>
+        </div>
     );
 };
 
