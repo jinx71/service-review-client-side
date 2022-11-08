6d87c1a268c002aff62db42f4e676586cc3bbc93
jinx71 <karim.sazed@gmail.com>
2022-11-09 03:08:50 +0600

    react-photo-app integrated

--- src/Components/Shared/Services/ServicesCard.js
+++ src/Components/Shared/Services/ServicesCard.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-
+import Ratings from 'react-ratings-declarative';
+import './serviceCard.css'
+import { PhotoProvider, PhotoView } from 'react-photo-view';
+import 'react-photo-view/dist/react-photo-view.css';
 const ServicesCard = ({ service, handleClick }) => {
     const trim = (abc) => {
         if (abc.length > 100) {
@@ -10,16 +13,39 @@ const ServicesCard = ({ service, handleClick }) => {
             return abc
         }
     }
+    const serviceRating = (rating, id) => {
+        let total = 0
+        let totalReviews = 0
+        for (const key in rating) {
+            total = total + parseInt(key) * parseInt(rating[key]);
+            totalReviews = totalReviews + parseInt(rating[key])
+        }
+        return (total / totalReviews)
+    }
     return (
         <div>
             <div className="card  border shadow-xl">
-                <figure><img src={service.image} alt="Album" /></figure>
+                <PhotoProvider>
+                    <PhotoView src={service.image}>
+                        <img src={service.image} className="homeServiceSectionImage" alt="" />
+                    </PhotoView>
+                </PhotoProvider>
                 <div className="card-body">
                     <h2 className="card-title">{service.serviceName}</h2>
-                    <p>{trim(service.Description)}</p>
+                    <p>{trim(service.Description_One)}</p>
                     <p className='text-lg font-bold'>Price : BDT {service.price}</p>
                     <div className="card-actions justify-between items-center">
-                        <p className='text-[#757575]'>Rating : {service.rating}</p>
+                        <Ratings
+                            rating={serviceRating(service.rating)}
+                            widgetRatedColors="orange"
+                            widgetSpacings='0px'
+                        >
+                            <Ratings.Widget widgetDimension="18px" />
+                            <Ratings.Widget widgetDimension="18px" />
+                            <Ratings.Widget widgetDimension="18px" />
+                            <Ratings.Widget widgetDimension="18px" />
+                            <Ratings.Widget widgetDimension="18px" />
+                        </Ratings>
                         <Link to={`/services/${service.id}`} className="btn btn-primary" onClick={() => handleClick(service.id)}>View Details</Link>
 
                     </div>
